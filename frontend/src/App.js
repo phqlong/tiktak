cb72468c9a4bb988475bf3dbcda1d16aced3d5b1
phqlong <phqlong2810@gmail.com>
2021-09-07 10:41:40 +0700

    Change hashrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Container } from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
